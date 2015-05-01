878453cd96b31afaebf9b9336541e5a09a949d61
Abdelrhman walid <abdelrhmanwalid1@gmail.com>
2015-05-01 07:07:10 +0200

    small fix

--- app/src/main/java/abdelrhman/moments/VideosFragment.java
+++ app/src/main/java/abdelrhman/moments/VideosFragment.java
@@ -3,13 +3,9 @@ package abdelrhman.moments;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ListFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import java.io.File;
 
@@ -18,18 +14,15 @@ public class VideosFragment extends ListFragment {
     Context context;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.moments_layout, container, false);
-
+    public void onStart() {
+        super.onStart();
         context = getActivity();
-
         File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "Moments");
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                return null;
+                return;
             }
         }
-
         Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
         String condition = MediaStore.Video.Media.DATA + " like?";
         String[] selectionArguments = new String[]{"%" + mediaStorageDir.toString() + "%"};
@@ -44,7 +37,5 @@ public class VideosFragment extends ListFragment {
         this.setListAdapter(videosAdapter);
 
         cursor.close();
-
-        return rootView;
     }
 }
